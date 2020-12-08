a22735ecf082f28ed082b24d62e5ce0a5992290c
Apurva Priyadarshi <apurva.priyadarshi@zenways.io>
2020-12-08 14:21:55 +0530

    Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Greet />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hello World!!!!!
         </p>
         <a
           className="App-link"
@@ -15,9 +18,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<br/>
+          Hello React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
